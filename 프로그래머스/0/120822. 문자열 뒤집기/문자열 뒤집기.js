a1116b1f6726e741ce793b2e399ddb4bbc46ad5a
Yes2eul <127367484+Yes2eul@users.noreply.github.com>
2024-03-07 12:43:58 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,7 +1,7 @@
 function solution(my_string) {
     let answer = "";
     if(my_string.length >= 1 && my_string.length <= 1000) {
-        answer = my_string.split("").reverse().join("");
+        answer = [...my_string].reverse().join("");
     }
     return answer;
 }
