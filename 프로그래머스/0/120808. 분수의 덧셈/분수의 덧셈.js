62ed23fdd637cf29468924b268f5d698d3c3a86a
Yes2eul <127367484+Yes2eul@users.noreply.github.com>
2024-03-07 14:48:15 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,16 @@
+function solution(numer1, denom1, numer2, denom2) {
+    // 분자
+    const a = (numer1 * denom2) + (numer2 * denom1)
+    // 분모
+    const b = denom1 * denom2
+    
+    function gcd(a, b) {
+        const remainder = a % b;  
+        if (remainder === 0)
+            return b;
+        else 
+            return gcd(b, remainder)
+        }
+    
+    return [a/gcd(a,b), b/gcd(a,b)]
+}
